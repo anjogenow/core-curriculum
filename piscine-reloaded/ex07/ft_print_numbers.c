3e6bfb126a563ff818b5eca75636ed01c37e7970
Anjo Genow <contact@anjogenow.com>
2023-10-20 15:04:09 +0200

    tabindent

--- piscine-reloaded/ex07/ft_print_numbers.c
+++ piscine-reloaded/ex07/ft_print_numbers.c
@@ -1,40 +1,39 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_print_alphabet.c                                :+:      :+:    :+:   */
+/*   ft_print_numbers.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: agenow <agenow@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/20 14:42:57 by agenow            #+#    #+#             */
-/*   Updated: 2023/10/20 14:59:38 by agenow           ###   ########.fr       */
+/*   Created: 2023/10/20 14:51:42 by agenow            #+#    #+#             */
+/*   Updated: 2023/10/20 15:03:20 by agenow           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 void	ft_putchar(char c);
 
-void	ft_print_alphabet(void)
+void	ft_print_numbers(void)
 {
 	char	c;
 
-	c = 'a';
-	while (c <= 'z')
+	c = '0';
+	while (c <= '9')
 	{
 		ft_putchar(c);
 		c++;
 	}
 }
-
 /*
 int	main(void)
 {
-	ft_print_alphabet();
-	return 0;
+	ft_print_numbers();
+	return (0);
 }
 
 #include <unistd.h>
 
 void	ft_putchar(char c)
 {
-	write(1, &c, 1);
+		write(1, &c, 1);
 }
 */
