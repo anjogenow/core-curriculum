e570e8fb9feeaea60c74eb4cfd1ec4077c9007fb
Anjo Genow <contact@anjogenow.com>
2023-10-21 16:02:16 +0200

    ex20 done

---
+++ piscine-reloaded/ex20/ft_strdup.c
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: agenow <agenow@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/21 15:39:48 by agenow            #+#    #+#             */
+/*   Updated: 2023/10/21 16:01:57 by agenow           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+char	*ft_strdup(char *src)
+{
+	int		i;
+	int		len;
+	char	*dest;
+
+	i = 0;
+	while (src[i])
+		i++;
+	len = i;
+	i = 0;
+	dest = (char *) malloc((len + 1) * sizeof(char));
+	if (!dest)
+		return (dest);
+	while (src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+/*
+#include <stdio.h>
+
+int main(void)
+{
+	char	*src;
+	char	*dest;
+
+	src = "Hello World!";
+	dest = ft_strdup(src);
+	printf("%s -> %s\n", src, dest);
+}
+*/
