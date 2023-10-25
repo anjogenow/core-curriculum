4686727212d7e3820a8057c89c1d9fdee8dc1bf0
Anjo Genow <code@anjogenow.com>
2023-10-25 19:26:48 +0200

    ft_memset.c

--- libft/ft_memset.c
+++ libft/ft_memset.c
@@ -6,11 +6,20 @@
 /*   By: agenow <agenow@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/25 11:39:26 by agenow            #+#    #+#             */
-/*   Updated: 2023/10/25 11:40:43 by agenow           ###   ########.fr       */
+/*   Updated: 2023/10/25 19:07:41 by agenow           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
 void	*memset(void *b, int c, size_t len)
 {
+	size_t			i;
+	unsigned char	*ptr;
 
+	i = 0;
+	ptr = (unsigned char *)b;
+	while (i < len)
+		ptr[i++] = (unsigned char)c;
+	return (b);
 }
